9968c84b8aa6f2079a1480a4178d291ec9a4f927
mkurtz <kurtz@dkrz.de>
2014-03-26 10:42:05 +0100

    Initial Import

---
+++ ckanext/b2find/helpers.py
@@ -0,0 +1,11 @@
+import ckan.plugins as p
+
+def featured_groups():
+    data_dict = {'sort': 'name', 'all_fields': True}
+    group_list = p.toolkit.get_action('group_list')
+    result = group_list({},data_dict)
+    return result
+
+def extras_to_exclude():
+    exclude_list = ['checksum','fulltext','ManagerVersion','MapperVersion','maxx','maxy','minx','miny','PublicationTimestamp','spatial','Version']
+    return exclude_list
---
+++ ckanext/b2find/plugin.py
@@ -0,0 +1,48 @@
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+import ckanext.b2find.helpers as helpers
+
+class B2FindPlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IFacets)
+    plugins.implements(plugins.ITemplateHelpers)
+
+    def get_helpers(self):
+        return{
+               'featured_groups': helpers.featured_groups,
+               'extras_to_exclude': helpers.extras_to_exclude
+              }
+
+    def update_config(self, config):
+        toolkit.add_public_directory(config, 'public')
+        toolkit.add_template_directory(config, 'templates')
+        toolkit.add_resource('fanstatic', 'b2find_theme')
+
+    def dataset_facets(self, facets_dict, package_type):
+        return self._facets(facets_dict)
+
+    def group_facets(self, facets_dict, group_type, package_type):
+        return self._facets(facets_dict)
+
+    def organization_facets(self, facets_dict, organization_type, package_type):
+        return self._facets(facets_dict)
+
+    def _facets(self, facets_dict):
+        # Deleted facets
+        if 'organization' in facets_dict:
+            del facets_dict['organization']
+        if 'license_id' in facets_dict:
+            del facets_dict['license_id']
+        if 'res_format' in facets_dict:
+            del facets_dict['res_format']
+        # Renamed facets
+        if 'groups' in facets_dict:
+            facets_dict['groups'] = 'Communities'
+        # New facets
+        facets_dict['author'] = 'Author'
+        facets_dict['extras_Discipline'] = 'Discipline'
+        facets_dict['extras_Language'] = 'Language'
+        facets_dict['extras_Origin'] = 'Origin'
+        facets_dict['extras_PublicationYear'] = 'Publication Year'
+        facets_dict['extras_Format'] = 'Format'
+        return facets_dict
