9772d586b8e21c3a0f1a43ea40aa556f24f0b54d
mkurtz <kurtz@dkrz.de>
2015-05-07 09:49:34 +0200

    Cumulative Update for new UI style
    Added new UI style
    Added missing image for "Sekretariat"
    Added collapsible facets
    Removed static html pages

--- ckanext/b2find/plugin.py
+++ ckanext/b2find/plugin.py
@@ -2,16 +2,18 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 import ckanext.b2find.helpers as helpers
 
+
 class B2FindPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IFacets)
     plugins.implements(plugins.ITemplateHelpers)
+    plugins.implements(plugins.IRoutes, inherit=True)
 
     def get_helpers(self):
-        return{
-               'featured_groups': helpers.featured_groups,
-               'extras_to_exclude': helpers.extras_to_exclude
-              }
+        return {
+            'featured_groups': helpers.featured_groups,
+            'extras_to_exclude': helpers.extras_to_exclude
+        }
 
     def update_config(self, config):
         toolkit.add_public_directory(config, 'public')
@@ -27,23 +29,41 @@ class B2FindPlugin(plugins.SingletonPlugin):
     def organization_facets(self, facets_dict, organization_type, package_type):
         return self._facets(facets_dict)
 
+    def after_map(self, map):
+        map.connect(
+            'legal',
+            '/legal',
+            controller='ckanext.b2find.controller:LegalController',
+            action='index'
+        )
+        map.connect(
+            'legal_action',
+            '/legal/{action}.html',
+            controller='ckanext.b2find.controller:LegalController'
+        )
+        map.connect(
+            'docs_search_guide',
+            '/docs/search-guide.html',
+            controller='ckanext.b2find.controller:DocsController',
+            action='search_guide'
+        )
+
+        return map
+
     def _facets(self, facets_dict):
         # Deleted facets
-        if 'organization' in facets_dict:
-            del facets_dict['organization']
-        if 'license_id' in facets_dict:
-            del facets_dict['license_id']
-        if 'res_format' in facets_dict:
-            del facets_dict['res_format']
+        facets_dict.pop('organization', None)
+        facets_dict.pop('license_id', None)
+        facets_dict.pop('res_format', None)
+
         # Renamed facets
         if 'groups' in facets_dict:
             facets_dict['groups'] = 'Communities'
+
         # New facets
         facets_dict['author'] = 'Creator'
         facets_dict['extras_Discipline'] = 'Discipline'
         facets_dict['extras_Language'] = 'Language'
         facets_dict['extras_Publisher'] = 'Publisher'
-        #facets_dict['extras_Origin'] = 'Origin'
-        #facets_dict['extras_PublicationYear'] = 'Publication Year'
-        #facets_dict['extras_Format'] = 'Format'
+
         return facets_dict
