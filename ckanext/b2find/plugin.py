f877ac34b1cc092868570186c946cfc5f00ff71d
mkurtz <kurtz@dkrz.de>
2015-05-07 09:49:34 +0200

    Remove commented lines

--- ckanext/b2find/plugin.py
+++ ckanext/b2find/plugin.py
@@ -65,8 +65,5 @@ class B2FindPlugin(plugins.SingletonPlugin):
         facets_dict['extras_Discipline'] = 'Discipline'
         facets_dict['extras_Language'] = 'Language'
         facets_dict['extras_Publisher'] = 'Publisher'
-        # facets_dict['extras_Origin'] = 'Origin'
-        # facets_dict['extras_PublicationYear'] = 'Publication Year'
-        # facets_dict['extras_Format'] = 'Format'
 
         return facets_dict
