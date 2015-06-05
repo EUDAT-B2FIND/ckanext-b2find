92b85dbacc3baa5defd537bffdc39660020c290a
Mikael Karlsson <i8myshoes@gmail.com>
2015-06-05 15:36:30 +0300

    Move facet limit to ng

--- ckanext/b2find/fanstatic/ng.js
+++ ckanext/b2find/fanstatic/ng.js
@@ -3,10 +3,14 @@ var app = angular.module('b2findApp', []);
 var controllers = {};
 
 controllers.BasicFacetController = function ($scope) {
+    $scope.facetMinLimit = 10;
+    $scope.facetMaxLimit = 100;
+
     // Copy properties over
     for (var k in basic_facets) {
         if (basic_facets.hasOwnProperty(k)) {
             $scope[k] = basic_facets[k];
+            $scope[k].limit = $scope.facetMinLimit;
         }
     }
 };
