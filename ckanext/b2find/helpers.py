4cca9735a38e291f3bc5e0a92ab513960c35c9d6
Mikael Karlsson <i8myshoes@gmail.com>
2015-09-29 14:28:51 +0300

    Clean up helpers

--- ckanext/b2find/helpers.py
+++ ckanext/b2find/helpers.py
@@ -1,11 +1,15 @@
 import ckan.plugins as p
 
+
 def featured_groups():
     data_dict = {'sort': 'name', 'all_fields': True}
     group_list = p.toolkit.get_action('group_list')
-    result = group_list({},data_dict)
+    result = group_list({}, data_dict)
     return result
 
+
 def extras_to_exclude():
-    exclude_list = ['B2SHARE-Domain','checksum','fulltext','identifiers','ManagerVersion','MapperVersion','maxx','maxy','minx','miny','oai_set','oai_identifier','PublicationTimestamp','spatial','TempCoverageBegin','TempCoverageEnd','TemporalCoverage:BeginDate','TemporalCoverage:EndDate']
+    exclude_list = ['B2SHARE-Domain', 'checksum', 'fulltext', 'identifiers', 'ManagerVersion', 'MapperVersion', 'maxx', 'maxy', 'minx', 'miny', 'oai_set',
+                    'oai_identifier', 'PublicationTimestamp', 'spatial', 'TempCoverageBegin', 'TempCoverageEnd', 'TemporalCoverage:BeginDate',
+                    'TemporalCoverage:EndDate']
     return exclude_list
