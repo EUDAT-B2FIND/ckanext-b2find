665da622e2c1dbee3e4c8b4ec535b98fa70f40b3
mkurtz <kurtz@dkrz.de>
2018-03-08 14:01:18 +0100

    Changed style of tags for package details template

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ckanext-b2find',
-    version='2.2.0',
+    version='2.3.0',
     description='CKAN extension for B2FIND',
     long_description=
     '''
@@ -10,7 +10,7 @@ setup(
     classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
     keywords='ckan ckanext b2find eudat',
     author='Michael Kurtz',
-    author_email='mkurtz@dkrz.de',
+    author_email='kurtz@dkrz.de',
     url='https://github.com/EUDAT-B2FIND/ckanext-b2find',
     license='AGPLv3',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
