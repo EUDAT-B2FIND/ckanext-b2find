effe843cb21486a0d40838aaacf8f4a0e1196690
Lenzma <fluegel@dkrz.de>
2021-06-28 15:34:51 +0200

    Added facet defs.

--- ckanext/b2find/plugin.py
+++ ckanext/b2find/plugin.py
@@ -12,3 +12,66 @@ class B2FindPlugin(plugins.SingletonPlugin):
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic',
             'b2find')
+    def dataset_facets(self, facets_dict, package_type):
+        return self._facets(facets_dict)
+
+    def group_facets(self, facets_dict, group_type, package_type):
+        return self._facets(facets_dict)
+
+    def organization_facets(self, facets_dict, organization_type, package_type):
+        return self._facets(facets_dict)
+
+    def after_map(self, map):
+        map.connect(
+            'legal',
+            '/legal',
+            controller='ckanext.b2find.controller:LegalController',
+            action='index'
+        )
+        map.connect(
+            'legal_action',
+            '/legal/{action}.html',
+            controller='ckanext.b2find.controller:LegalController'
+        )
+        map.connect(
+            'help_action',
+            '/help/{action}.html',
+            controller='ckanext.b2find.controller:HelpController'
+        )
+        map.connect(
+            'guidelines',
+            '/guidelines',
+            controller='ckanext.b2find.controller:GuidelinesController',
+            action='index'
+        )
+        map.connect(
+            'guidelines_action',
+            '/guidelines/{action}.html',
+            controller='ckanext.b2find.controller:GuidelinesController'
+        )
+
+        return map
+
+    def _facets(self, facets_dict):
+        # Deleted facets
+        facets_dict.pop('organization', None)
+        facets_dict.pop('license_id', None)
+        facets_dict.pop('res_format', None)
+
+        # Renamed facets
+        if 'groups' in facets_dict:
+            facets_dict['groups'] = 'Communities'
+        if 'tags' in facets_dict:
+            facets_dict['tags'] = 'Keywords'
+
+        # New facets
+        facets_dict['author'] = 'Creator'
+#        facets_dict['instrument'] = 'Instrument'
+        facets_dict['extras_Discipline'] = 'Discipline'
+        facets_dict['extras_Language'] = 'Language'
+        facets_dict['extras_Publisher'] = 'Publisher'
+        facets_dict['extras_Contributor'] = 'Contributor'
+        facets_dict['extras_ResourceType'] = 'ResourceType'
+        facets_dict['extras_OpenAccess'] = 'OpenAccess'
+
+        return facets_dict
