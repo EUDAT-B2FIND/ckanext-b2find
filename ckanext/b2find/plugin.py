c49ff1dddc2f8ca0861bb7328b1873109e5ace04
Lenzma <fluegel@dkrz.de>
2021-06-28 15:56:19 +0200

    Added helpers def.

--- ckanext/b2find/plugin.py
+++ ckanext/b2find/plugin.py
@@ -1,17 +1,47 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+import ckanext.b2find.helpers as helpers
+
+def legacy_pager(self, *args, **kwargs):
+    kwargs.update(
+        format=u"<div class='pagination-wrapper pagination pagination-centered'><ul>"
+        "$link_previous ~2~ $link_next</ul></div>",
+        symbol_previous=u'«', symbol_next=u'»',
+        curpage_attr={'class': 'active'}, link_attr={}
+    )
+    from ckan.lib.helpers import Page
+    return super(Page, self).pager(*args, **kwargs)
 
 
 class B2FindPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IFacets)
+    #plugins.implements(plugins.ITemplateHelpers)
+    #plugins.implements(plugins.IRoutes, inherit=True)
 
     # IConfigurer
+
+    def get_helpers(self):
+        return {
+            'featured_groups': helpers.featured_groups,
+            'extras_to_exclude': helpers.extras_to_exclude,
+            'link_fields': helpers.link_fields,
+            'strip_brackets': helpers.strip_brackets,
+            'equals_ignore_case': helpers.equals_ignore_case,
+            'split_extra': helpers.split_extra
+        }
 
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic',
             'b2find')
+        
+        if 'ckan.base_templates_folder' in config and config['ckan.base_templates_folder'] == 'templates-bs2':
+            from ckan.lib.helpers import Page
+            Page.pager = legacy_pager
+        return config
+
     def dataset_facets(self, facets_dict, package_type):
         return self._facets(facets_dict)
 
