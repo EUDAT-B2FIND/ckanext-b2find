6659fca9bebbfc52efcb31a17c4a14e0ef806542
Carsten Ehbrecht <ehbrecht@dkrz.de>
2022-02-01 20:49:56 +0100

    clean up helpers

--- ckanext/b2find/helpers.py
+++ ckanext/b2find/helpers.py
@@ -1,13 +1,3 @@
-import ckan.plugins as p
-
-
-def featured_groups():
-    data_dict = {'sort': 'name', 'all_fields': True}
-    group_list = p.toolkit.get_action('group_list')
-    result = group_list({}, data_dict)
-    return result
-
-
 def extras_to_exclude():
     exclude_list = [
         'B2SHARE-Domain', 'checksum', 'fulltext', 'identifiers',
@@ -22,11 +12,6 @@ def extras_to_exclude():
         'TemporalCoverage:BeginDate',
         'TemporalCoverage:EndDate']
     return exclude_list
-
-
-def link_fields():
-    field_list = ['MetaDataAccess', 'PID', 'DOI']
-    return field_list
 
 
 def strip_brackets(*args, **kw):
