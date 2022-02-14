e745d5b4114980add5fab639e57ac72bdf2e98e0
Carsten Ehbrecht <ehbrecht@dkrz.de>
2022-02-14 22:38:47 +0100

    working on reactjs facet component

--- ckanext/b2find/assets/app.js
+++ ckanext/b2find/assets/app.js
@@ -1,52 +1,152 @@
 'use strict';
 
-// import React, { useState, useEffect } from 'react';
-
 const e = React.createElement;
 
-function FacetsComponent() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState([]);
+class SelectSort extends React.Component {
+  constructor(props) {
+    super(props);
 
-  // Note: the empty deps array [] means
-  // this useEffect will run once
-  // similar to componentDidMount()
-  useEffect(() => {
-    fetch("/b2find/query")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setItems(result);
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      )
-  }, [])
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-  if (error) {
-    return "Error";// <div>Error: {error.message}</div>;
-  } else if (!isLoaded) {
-    return "Loading..."; // <div>Loading...</div>;
-  } else {
+  handleChange(event) {
+    this.props.handleSortChange(event.target.value);
+  }
+
+  render() {
     return (
-      "items"
-      // <ul>
-      //   {items.map(item => (
-      //     <li key={item.val}>
-      //       {item.val} {item.count}
-      //     </li>
-      //   ))}
-      // </ul>
+      <select className="facet-filter pull-right" onChange={this.handleChange}
+        defaultValue={this.props.sort}>
+        <option value="ia">A-Z</option>
+        <option value="id">Z-A</option>
+        <option value="ca">1-9</option>
+        <option value="cd">9-1</option>
+      </select>
+    )
+  }
+}
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  render() {
+    return (
+      <input type="text" placeholder="Filter" className="facet-filter"/>
+    )
+  }
+}
+
+class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    };
+
+  }
+
+  render() {
+    return (
+      <p className="module-footer">
+        <a href="" className="read-more">Less</a>
+        <a href="" className="read-more pull-right">More</a>
+      </p>
+    )
+  }
+}
+
+class Facet extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+      sort: "cd",
+      limit: 5,
+    };
+
+    this.handleSortChange = this.handleSortChange.bind(this);
+  }
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.sort !== this.state.sort) {
+      this.fetchData();
+      console.log("fetch new data");
+    }
+  }
+
+  handleSortChange(sort) {
+    this.setState({
+      sort: sort
+    });
+    console.log("sort changed", sort);
+  }
+
+  fetchData() {
+    const field = this.props.facet_id;
+    const sort = this.state.sort;
+    const limit = this.state.limit;
+    const url = "/b2find/query?field="+field+"&sort="+sort+"&limit="+limit;
+
+    fetch(url)
+      .then(result => result.json())
+      .then(result => {
+        console.log(url);
+        this.setState({
+          items: result.items,
+          isLoaded: true
+        })
+      });
+    }
+
+  render() {
+    const facet_id = this.facet_id
+    const items = this.state.items;
+    const isLoaded = this.state.isLoaded;
+
+    if (!isLoaded) return (
+      <div>
+        <h1> Loading ... </h1>
+      </div>
+    );
+    return (
+      <div id="facet_tags" className="collapse">
+        <SearchBar/>
+        <SelectSort
+          sort={this.state.sort}
+          handleSortChange={this.handleSortChange}/>
+        <nav aria-label="">
+          <ul className="list-unstyled nav nav-simple nav-facet">
+            {items.map((item, index) => (
+              <li key={index} className="nav-item">
+                <a href="" title={item.val}>
+                  {item.val.substring(0,20)} <span className="badge">{item.count}</span>
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <Footer/>
+      </div>
     );
   }
 }
 
-const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(FacetsComponent), domContainer);
+ReactDOM.render(
+  <Facet facet_id="tags" />,
+  document.getElementById('b2find_facet')
+);
