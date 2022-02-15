765bcb8264d9766bb73da7ae480661da7b843f42
Carsten Ehbrecht <ehbrecht@dkrz.de>
2022-02-15 19:55:50 +0100

    update facets

--- ckanext/b2find/assets/app.js
+++ ckanext/b2find/assets/app.js
@@ -1,5 +1,23 @@
 'use strict';
 
+function Header(props) {
+  return (
+    <h2
+      className="module-heading">
+      <i className="fa fa-filter"></i>
+      { props.title }
+    </h2>
+  )
+}
+
+function SearchBar(props) {
+  const [value, setValue] = React.useState();
+
+  return (
+    <input type="text" placeholder="Filter" className="facet-filter"/>
+  )
+}
+
 function SelectSort(props) {
   return (
     <select
@@ -14,34 +32,12 @@ function SelectSort(props) {
   )
 }
 
-function SearchBar(props) {
-  const [value, setValue] = React.useState();
-
-  return (
-    <input type="text" placeholder="Filter" className="facet-filter"/>
-  )
-}
-
 function Footer(props) {
   return (
     <p className="module-footer">
       <a href="" className="read-more">Less</a>
       <a href="" className="read-more pull-right">More</a>
     </p>
-  )
-}
-
-function Header(props) {
-  return (
-    <h2
-      className="module-heading accordion-toggle"
-      data-toggle="collapse"
-      data-target="#facet_tags"
-      title={props.title}>
-      <i className="fa fa-filter" aria-hidden="true"></i>
-      { props.title }
-      <i className="fa fa-chevron-down pull-right" aria-hidden="true"></i>
-    </h2>
   )
 }
 
@@ -67,7 +63,7 @@ function Facet(props) {
     <h1> Loading ... </h1>
   );
   return (
-    <section className="nav-facet nav-facet-tertiary">
+    <section className="module module-narrow module-shallow">
         <Header title={props.title}/>
         <SearchBar/>
         <SelectSort
@@ -78,7 +74,9 @@ function Facet(props) {
             {items.map((item, index) => (
               <li key={index} className="nav-item">
                 <a href="" title={item.val}>
-                  {item.val.substring(0,20)} <span className="badge">{item.count}</span>
+                  <span className="item-label">{item.val.substring(0,20)}</span>
+                  <span className="hidden separator"> - </span>
+                  <span className="item-count badge">{item.count}</span>
                 </a>
               </li>
             ))}
