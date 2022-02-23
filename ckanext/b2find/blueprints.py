df26421fdd5cf8867010dada078c954e1674ea3a
Carsten Ehbrecht <ehbrecht@dkrz.de>
2022-02-23 01:13:43 +0100

    clean up

--- ckanext/b2find/blueprints.py
+++ ckanext/b2find/blueprints.py
@@ -6,24 +6,9 @@ import requests
 b2find = Blueprint('b2find', __name__)
 
 
-def _translate_sort(sort=None):
-    if sort == "cd":
-        sort_param = {"count": "desc"}
-    elif sort == "ca":
-        sort_param = {"count": "asc"}
-    elif sort == "id":
-        sort_param = {"index": "desc"}
-    elif sort == "ia":
-        sort_param = {"index": "asc"}
-    else:
-        sort_param = {"count": "desc"}
-    return sort_param
-
-
 @b2find.route('/b2find/query', endpoint='query_facets',
               methods=['POST'])
 def query_facets():
-    print("query")
     content_type = request.headers.get('Content-Type')
     if (content_type != 'application/json'):
         return {}
