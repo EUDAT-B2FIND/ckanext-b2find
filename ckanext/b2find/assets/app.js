c8f1c87570cc90aaccf9dacf756168a215c95acd
Carsten Ehbrecht <ehbrecht@dkrz.de>
2022-03-01 21:43:57 +0100

    added initial map

--- ckanext/b2find/assets/app.js
+++ ckanext/b2find/assets/app.js
@@ -362,14 +362,45 @@ function TimeRangeFacet(props) {
   );
 }
 
+function MyMap() {
+  // Create our map tile layer:
+  const MAP_TILE = L.tileLayer(`https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`, {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  });
+
+  // Define the styles that are to be passed to the map instance:
+  const mapStyles = {
+    overflow: "hidden",
+    width: "100%",
+    height: "50vh"
+  };
+  // Define an object literal with params that will be passed to the map:
+  const mapParams = {
+    center: [37.0902, -95.7129],
+    zoom: 3,
+    zoomControl: false,
+    maxBounds: L.latLngBounds(L.latLng(-150, -240), L.latLng(150, 240)),
+    layers: [MAP_TILE]
+  };
+
+  React.useEffect(() => {
+    const map = L.map("map", mapParams);
+  }, [])
+
+  return (
+      <div id="map" style={mapStyles}></div>
+  )
+}
+
 function MapFacet(props) {
-  const id = "map_facet_" + props.field;
+  const id = "facet_" + props.field;
   const title = props.title;
+  const field = props.field;
+
   return (
     <section className="module module-narrow module-shallow">
       <Header title={title}/>
-        <div id={id} className="collapse">
-        </div>
+        <MyMap />
     </section>
   );
 }
