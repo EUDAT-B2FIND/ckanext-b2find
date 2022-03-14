5bf908a4e18a0589e7c9551a9f4e7a72fa086abd
Carsten Ehbrecht <ehbrecht@dkrz.de>
2022-03-14 20:51:12 +0100

    added dataset map

--- ckanext/b2find/assets/dataset.js
+++ ckanext/b2find/assets/dataset.js
@@ -1,1 +1,54 @@
 "use strict";
+
+function DatasetMap() {
+  const [map, setMap] = React.useState();
+  const [zoom, setZoom] = React.useState(0);
+  const [center, setCenter] = React.useState([0.0, 0.0]);
+
+  // create state ref that can be accessed in OpenLayers onclick callback function
+  //  https://stackoverflow.com/a/60643670
+  const mapRef = React.useRef();
+  mapRef.current = map;
+
+  // Define the styles that are to be passed to the map instance:
+  const mapStyles = {
+    // overflow: "hidden",
+    width: "100%",
+    height: "280px"
+  };
+
+  const stamen = new ol.layer.Tile({
+    source: new ol.source.Stamen({
+      layer: 'toner',
+    }),
+  });
+
+  const osm = new ol.layer.Tile({
+    source: new ol.source.OSM()
+  });
+
+  React.useEffect(() => {
+    //console.log("Initialised for the first time");
+    const myMap = new ol.Map({
+        target: mapRef.current,
+        layers: [
+          stamen,
+        ],
+        view: new ol.View({
+          center: ol.proj.fromLonLat(center),
+          zoom: zoom,
+        })
+      });
+
+    setMap(myMap);
+  }, [])
+
+  return (
+      <div ref={mapRef} style={mapStyles}></div>
+  )
+}
+
+ReactDOM.render(
+  <DatasetMap/>,
+  document.getElementById('b2find_map')
+);
